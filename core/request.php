29ca8453f9593321bdba5c76c885231912cd609a
Eric Ingram <eric@schema.io>
2015-07-20 19:09:35 -0700

    consider http forwarded proto in https detection

--- core/request.php
+++ core/request.php
@@ -477,7 +477,8 @@ class Request
 
         // Secure/HTTPS
         $request['secure'] = !isset($_SERVER['HTTP_HOST'])
-            || (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && $_SERVER['HTTPS'] !== 'off');
+            || (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && $_SERVER['HTTPS'] !== 'off')
+            || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https');
 
         return $request;
     }
